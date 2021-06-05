373914f99606fddc92585414c0f79d1c859bde05
osmanpampal28 <55186715+osmanpampal28@users.noreply.github.com>
2021-06-05 09:41:06 +0300

    ATM uygulaması

---
+++ atm/src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String username,password;
+        Scanner scanner=new Scanner(System.in);
+
+        int right=3,balance=1500,select;
+        while(right>0){
+            System.out.println("Kullanıcı adı : ");
+            username=scanner.nextLine();
+            System.out.println("Parolanız : ");
+            password=scanner.nextLine();
+
+            if(username.equals("patika") && password.equals("dev123")){
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do{
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = scanner.nextInt();
+
+                    switch (select){
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            int price = scanner.nextInt();
+                            balance += price;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            int price2 = scanner.nextInt();
+                            if (price2 > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= price2;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                        default:
+                            System.out.println("Üzgünüm!");
+                            break;
+                    }
+                }while(select!=4);
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+            }
+            else{
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+    }
+}
